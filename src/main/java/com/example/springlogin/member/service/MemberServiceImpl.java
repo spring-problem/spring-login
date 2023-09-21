e7aadc3814a6696416ff1541876ec5c3547b9bb6
junyoung-choe <wjdghkswnsdu@naver.com>
2023-09-22 01:55:33 +0900

    fix: Member service 쿼리문을 이용한 성능 향상과 null인 경우의 예외처리 추가

--- src/main/java/com/example/springlogin/member/service/MemberServiceImpl.java
+++ src/main/java/com/example/springlogin/member/service/MemberServiceImpl.java
@@ -2,43 +2,38 @@ package com.example.springlogin.member.service;
 
 import com.example.springlogin.member.domain.Member;
 import com.example.springlogin.member.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+// Autowired + final
+@RequiredArgsConstructor
 @Service
 public class MemberServiceImpl implements MemberService {
     final MemberRepository memberRepository;
 
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
     @Override
     public boolean signUp(signUpParam param) {
-        Member member = new Member(param.getEmail(), param.getPassword());
 
-        List<Member> list = memberRepository.findAll();
-        for (Member tmp : list) {
-            if(tmp.equals(member))
-                return false;
+
+        Member tmp = memberRepository.findByEmail(param.getEmail());
+        if(tmp == null) {
+            throw new RuntimeException("중복되는 아이디가 존재합니다.");
         }
-
-        memberRepository.save(member);
+        // 회원가입 성공
+        memberRepository.save(tmp);
         return true;
+
     }
 
     @Override
     public String login(loginParam param) {
-        Member member = new Member(param.getEmail(), param.getPassword());
 
-        List<Member> list = memberRepository.findAll();
-        for (Member tmp : list) {
-            if(tmp.equals(member))
-                return param.getEmail();
+        Member tmp = memberRepository.findByEmailAndPassword(param.getEmail(), param.getPassword());
+        if (tmp == null) {
+            throw new RuntimeException("아이디와 비밀번호를 확인해 주세요");
         }
-
-        return null;
+        // 로그인 성공
+        return tmp.getEmail();
     }
 }
