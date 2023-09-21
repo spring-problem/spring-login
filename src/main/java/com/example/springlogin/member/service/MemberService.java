d116b281670958a57588479b296fd9779a2cb42e
juhyun-99 <juhyeon042@naver.com>
2023-09-21 17:26:03 +0900

    feat: MemberService 및 MemberRepository 추가

--- src/main/java/com/example/springlogin/member/service/MemberService.java
+++ src/main/java/com/example/springlogin/member/service/MemberService.java
@@ -1,4 +1,9 @@
 package com.example.springlogin.member.service;
 
+import com.example.springlogin.member.request.JoinRequest;
+import com.example.springlogin.member.request.LoginRequest;
+
 public interface MemberService {
+    boolean join(JoinRequest joinRequest);
+    boolean login(LoginRequest loginRequest);
 }
--- src/main/java/com/example/springlogin/member/service/MemberServiceImpl.java
+++ src/main/java/com/example/springlogin/member/service/MemberServiceImpl.java
@@ -1,8 +1,37 @@
 package com.example.springlogin.member.service;
 
+import com.example.springlogin.member.domain.Member;
+import com.example.springlogin.member.param.JoinParam;
+import com.example.springlogin.member.repository.MemberRepository;
+import com.example.springlogin.member.request.JoinRequest;
+import com.example.springlogin.member.request.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberServiceImpl implements MemberService {
 
+    @Autowired
+    MemberRepository memberRepository;
+
+    public boolean join(JoinRequest joinRequest){
+        JoinParam param = new JoinParam();
+        param.setEmail(joinRequest.getEmail());
+        param.setPassword(joinRequest.getEmail());
+        Member tmpMember = memberRepository.findByEmail(param.getEmail());
+        if(tmpMember != null) return false; //같은 이메일로 가입한 사람이 있을 경우 return false;
+
+        //없을 경우 Member에 member 등록
+        Member member = new Member(joinRequest.getEmail(), joinRequest.getPassword());
+        memberRepository.save(member);
+        return true;
+    }
+
+    public boolean login(LoginRequest loginRequest){
+        //이메일과 password로 member찾기
+        Member member = memberRepository.findByEmailAndPassword(loginRequest.getEmail(), loginRequest.getPassword());
+        if(member == null)  return false; //없으면 false
+        else return true; //있으면 true
+
+    }
 }
