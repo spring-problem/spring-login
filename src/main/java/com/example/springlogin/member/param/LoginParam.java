6234dc0e0b04cd6c9b08b60b4c34804bcac6aa8a
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-09-24 22:24:03 +0900

    refactor: request 삭제 및 Service 파라미터 통일

--- src/main/java/com/example/springlogin/member/param/LoginParam.java
+++ src/main/java/com/example/springlogin/member/param/LoginParam.java
@@ -1,11 +1,11 @@
-package com.example.springlogin.member.request;
+package com.example.springlogin.member.param;
 
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class LoginRequest {
+public class LoginParam {
     private String email;
     private String password;
 }
--- src/main/java/com/example/springlogin/member/service/MemberService.java
+++ src/main/java/com/example/springlogin/member/service/MemberService.java
@@ -2,10 +2,9 @@ package com.example.springlogin.member.service;
 
 import com.example.springlogin.member.domain.Member;
 import com.example.springlogin.member.param.JoinParam;
-import com.example.springlogin.member.request.JoinRequest;
-import com.example.springlogin.member.request.LoginRequest;
+import com.example.springlogin.member.param.LoginParam;
 
 public interface MemberService {
     void join(JoinParam param);
-    Member login(LoginRequest loginRequest);
+    Member login(LoginParam loginRequest);
 }
