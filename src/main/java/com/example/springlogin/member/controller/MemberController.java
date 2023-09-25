ac25bfbbefa1d0f9ccbf8f4d4d97993ffca1ab33
bluepeng9 <bluepeng9@gmail.com>
2023-09-25 23:23:18 +0900

    feat: getHomepage 추가

--- src/main/java/com/example/springlogin/member/controller/MemberController.java
+++ src/main/java/com/example/springlogin/member/controller/MemberController.java
@@ -4,11 +4,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public interface MemberController {
+
+    @GetMapping("/")
+    default String getHomepage(
+            @CookieValue(value = "loginByCookie", required = false) boolean loginByCookie,
+            Model model
+    ) {
+        model.addAttribute("loginByCookie", loginByCookie);
+        return "index";
+    }
 
     @GetMapping("/login")
     String getLoginPage(HttpServletRequest request, HttpServletResponse response, Model model);
