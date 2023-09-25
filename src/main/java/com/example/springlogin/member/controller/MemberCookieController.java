fb12a903efc7445f87291438370fcd5ad9c9fbe7
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-09-25 22:19:46 +0900

    feat: index 페이지 이동 추가 및 cookie 설정

--- src/main/java/com/example/springlogin/member/controller/MemberCookieController.java
+++ src/main/java/com/example/springlogin/member/controller/MemberCookieController.java
@@ -4,12 +4,10 @@ import com.example.springlogin.member.controller.request.LoginRequest;
 import com.example.springlogin.member.domain.Member;
 import com.example.springlogin.member.param.LoginParam;
 import com.example.springlogin.member.service.MemberService;
-import com.example.springlogin.member.service.MemberServiceImpl;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 
@@ -17,6 +15,12 @@ import org.springframework.ui.Model;
 public class MemberCookieController implements MemberController {
     @Autowired
     private MemberService service;
+
+    @Override
+    public String getIndexPage(HttpServletRequest request, HttpServletResponse response) {
+        return "/index";
+    }
+
     @Override
     public String getLoginPage(HttpServletRequest request, HttpServletResponse response, Model model) {
         model.addAttribute("loginRequest", new LoginRequest());
@@ -37,18 +41,34 @@ public class MemberCookieController implements MemberController {
                 .password(loginRequest.getPassword())
                 .build();
         System.out.println(param.toString());
-        Member member = service.login(param);
-        if(member != null){
-            HttpSession session = request.getSession();
-            session.setAttribute("loginByCookie", true);
-            return "/index";
+        try{
+            Member member = service.login(param);
+            if(member != null){
+                HttpSession session = request.getSession();
+                Cookie cookie = new Cookie("loginByCookie" , "true");
+                cookie.setMaxAge(60*60*24*400);
+                response.addCookie(cookie);
+                return "/index";
+            }
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
         }
+
         return "/login";
     }
 
     @Override
     public String logout(HttpServletRequest request, HttpServletResponse response) {
-        request.getSession().invalidate();
-        return "redirect:/index";
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for (Cookie cookie:cookies) {
+                if(cookie.getName().equals("loginByCookie")){
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                    break;
+                }
+            }
+        }
+        return "/index";
     }
 }
