e7efb6a79f8b67043c93aa6cbc4810f8a828e94c
junyoung-choe <wjdghkswnsdu@naver.com>
2023-09-26 00:44:30 +0900

    refactor: 피드백 후 cookieController 수정

--- src/main/java/com/example/springlogin/member/service/MemberService.java
+++ src/main/java/com/example/springlogin/member/service/MemberService.java
@@ -1,8 +1,7 @@
 package com.example.springlogin.member.service;
 
 import com.example.springlogin.member.domain.Member;
-import com.example.springlogin.member.param.JoinParam;
-import com.example.springlogin.member.param.LoginParam;
+
 
 public interface MemberService {
     void join(JoinParam param);
