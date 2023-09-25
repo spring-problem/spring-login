dcb3625c98d1859a8b0d065a435af0527efd70dd
junyoung-choe <wjdghkswnsdu@naver.com>
2023-09-25 22:55:40 +0900

    refactor: 컨트롤러 Config 주입으로 수정

--- src/main/java/com/example/springlogin/config/ApplicationConfig.java
+++ src/main/java/com/example/springlogin/config/ApplicationConfig.java
@@ -7,10 +7,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
-
-//    @Bean
-//    MemberController memberController() {
-//        return new MemberController();
-//    }
+    @Bean
+    MemberController memberController() {
+        return new MemberCookieController();
+    }
 
 }
