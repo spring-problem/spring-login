7905728975dd41eb04e435fa2249501c20136235
junyoung-choe <wjdghkswnsdu@naver.com>
2023-09-26 00:44:30 +0900

    refactor: 피드백 후 cookieController 수정

--- src/main/java/com/example/springlogin/member/controller/MemberCookieController.java
+++ src/main/java/com/example/springlogin/member/controller/MemberCookieController.java
@@ -7,27 +7,44 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Component
+@ComponentScan
 public class MemberCookieController implements MemberController {
 
     @Autowired
     private MemberService memberService;
 
-    @GetMapping("/")
-    public String getHome() {
-        return "index";
-    }
     @Override
     @GetMapping("login")
     public String getLoginPage(HttpServletRequest request, HttpServletResponse response, Model model) {
-        return "login";
+        Cookie[] cookies = request.getCookies();
+
+        if(cookies != null) {
+            for(Cookie cookie : cookies) {
+                if("loginByCookie".equals(cookie.getName())) {
+                    // 로그인된 상태라면 로그인 페이지로 이동하는게 아닌 다시 원래의 login으로 이동..
+                    return "/index";
+                }
+            }
+        }
+        // 로그인이 되어있지 않다면 로그인 페이지로 이동한다...
+        return "/login";
     }
+    // 인터페이스는 여러가지 컨트롤러를 사용할수 있기에 근본에 충실해서 구현하는 것도 중요하다
+//    public String getLoginPage(@CookieValue(value = "loginByCookie", required = false) boolean loginByCookie) {
+//        if (loginByCookie) {
+//            // 쿠키가 존재하는 경우
+//            return "/index";
+//        } else {
+//            // 쿠키가 존재하지 않는 경우
+//            return "/login";
+//        }
+//    }
 
     @Override
     @PostMapping("login")
@@ -41,22 +58,22 @@ public class MemberCookieController implements MemberController {
         Member member = memberService.login(loginParam);
         // 로그인에 성공 했다면 !
         if (member != null) {
-            Cookie cookie = new Cookie("email", member.getEmail());
+            Cookie cookie = new Cookie("loginByCookie", member.getEmail());
             // 쿠키 시간 디폴트로 설정
             response.addCookie(cookie);
         }
 
-        model.addAttribute("email", member.getEmail());
+        //model.addAttribute("loginByCookie", member.getEmail());
 
-
-        return "index";
+        return "/index";
     }
 
     @Override
     @PostMapping("logout")
     public String logout(HttpServletRequest request, HttpServletResponse response) {
         // 쿠키 시간을 만료시켜서 쿠키를 제거한다
-        Cookie cookie = new Cookie("email", null);
+        // index 파일로 매핑되고 view에 들어가기 전에 컨트롤러에서 쿠키를 확인하고 주입해주기에 여기서는 쿠키 만료만 진행
+        Cookie cookie = new Cookie("loginByCookie", null);
         cookie.setMaxAge(0);
         response.addCookie(cookie);
 
