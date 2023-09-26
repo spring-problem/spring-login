86f87ec5cb877f67c7ff4628a09c8ebaa3366c3e
junyoung-choe <wjdghkswnsdu@naver.com>
2023-09-26 23:21:33 +0900

    refactor: Config Bean 주입과 Lombok을 사용한 생성자 생성

--- src/main/java/com/example/springlogin/config/ApplicationConfig.java
+++ src/main/java/com/example/springlogin/config/ApplicationConfig.java
@@ -2,14 +2,15 @@ package com.example.springlogin.config;
 
 import com.example.springlogin.member.controller.MemberController;
 import com.example.springlogin.member.controller.MemberCookieController;
+import com.example.springlogin.member.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
     @Bean
-    MemberController memberController() {
-        return new MemberCookieController();
-    }
+    MemberController memberCookieController(MemberService memberService) { // 빈으로 만들어 주겠다
 
+        return new MemberCookieController(memberService); // @Bean 리턴하는 값을 스프링의 빈 컨테이너에 넣겠다
+    }
 }
