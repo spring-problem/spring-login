74c16a60995383463cc5a87174560d7fb3cdd280
bluepeng9 <bluepeng9@gmail.com>
2023-10-06 00:27:44 +0900

    feat: 로그인 실패 시 id 유지 기능 구현

--- src/main/java/com/example/springlogin/member/service/MemberService.java
+++ src/main/java/com/example/springlogin/member/service/MemberService.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface MemberService {
     void join(JoinParam param);
     Optional<Member> login(LoginParam loginRequest);
+
+    Optional<Member> getLoginUserById(Long id);
 }
