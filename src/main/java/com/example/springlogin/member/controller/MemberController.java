df2be0a9178f7e72970535b9a5b30562b7421299
bluepeng9 <bluepeng9@gmail.com>
2023-10-08 06:32:02 +0900

    feat: 회원가입 페이지 추가

--- src/main/java/com/example/springlogin/member/controller/MemberController.java
+++ src/main/java/com/example/springlogin/member/controller/MemberController.java
@@ -1,10 +1,12 @@
 package com.example.springlogin.member.controller;
 
+import com.example.springlogin.member.controller.request.JoinRequest;
 import com.example.springlogin.member.controller.request.LoginRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +15,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 public interface MemberController {
 
     @GetMapping("/")
-    String getHomepage(HttpServletRequest request, HttpServletResponse response, Model model);
+    default String getHomepage(
+            @CookieValue(value = "loginByCookie", required = false) boolean loginByCookie,
+            Model model
+    ) {
+        model.addAttribute("loginByCookie", loginByCookie);
+        return "index";
+    }
 
     @GetMapping("/login")
     String getLoginPage(HttpServletRequest request, HttpServletResponse response, Model model);
@@ -24,5 +32,10 @@ public interface MemberController {
     @PostMapping("/logout")
     String logout(HttpServletRequest request, HttpServletResponse response);
 
+    @GetMapping("/join")
+    String getJoinPage(HttpServletRequest request, HttpServletResponse response, Model model);
+
+    @PostMapping("/join")
+    String join(@ModelAttribute JoinRequest joinRequest, HttpServletRequest request, HttpServletResponse response);
 
 }
