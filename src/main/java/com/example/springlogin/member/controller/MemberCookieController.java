c7c780df827126b02ddf6a0e3497f3dfac474048
bluepeng9 <bluepeng9@gmail.com>
2023-10-08 06:32:02 +0900

    feat: 회원가입 페이지 추가

--- src/main/java/com/example/springlogin/member/controller/MemberCookieController.java
+++ src/main/java/com/example/springlogin/member/controller/MemberCookieController.java
@@ -1,8 +1,10 @@
 package com.example.springlogin.member.controller;
 
+import com.example.springlogin.member.controller.request.JoinRequest;
 import com.example.springlogin.member.controller.request.LoginRequest;
 import com.example.springlogin.member.domain.Member;
 import com.example.springlogin.member.service.MemberService;
+import com.example.springlogin.member.service.param.JoinParam;
 import com.example.springlogin.member.service.param.LoginParam;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,29 +18,8 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MemberCookieController implements MemberController {
 
+
     private final MemberService memberService;
-    private final String loginCookieName = "userId";
-
-    @Override
-    public String getHomepage(HttpServletRequest request, HttpServletResponse response, Model model) {
-        Cookie[] cookies = request.getCookies();
-
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (loginCookieName.equals(cookie.getName())) {
-                    model.addAttribute("loginByCookie", true);
-                    String userId = cookie.getValue();
-                    Optional<Member> findUser = memberService.getLoginUserById(Long.parseLong(userId));
-                    if (findUser.isEmpty()) {
-                        throw new RuntimeException("유저가 존재하지 않습니다.");
-                    }
-                    model.addAttribute("email", findUser.get().getEmail());
-                }
-            }
-        }
-
-        return "index";
-    }
 
     @Override
     public String getLoginPage(HttpServletRequest request, HttpServletResponse response, Model model) {
@@ -47,7 +28,7 @@ public class MemberCookieController implements MemberController {
 
         if (cookies != null) {
             for (Cookie cookie : cookies) {
-                if (loginCookieName.equals(cookie.getName())) {
+                if ("loginByCookie".equals(cookie.getName())) {
                     return "redirect:/";
                 }
             }
@@ -64,7 +45,7 @@ public class MemberCookieController implements MemberController {
 
         Optional<Member> member = memberService.login(loginParam);
         if (member.isPresent()) {
-            Cookie cookie = new Cookie(loginCookieName, member.get().getId().toString());
+            Cookie cookie = new Cookie("loginByCookie", member.get().getEmail());
             response.addCookie(cookie);
             return "redirect:/";
         }
@@ -73,10 +54,26 @@ public class MemberCookieController implements MemberController {
 
     @Override
     public String logout(HttpServletRequest request, HttpServletResponse response) {
-        Cookie cookie = new Cookie(loginCookieName, null);
+        Cookie cookie = new Cookie("loginByCookie", null);
         cookie.setMaxAge(0);
         response.addCookie(cookie);
         return "redirect:/";
     }
 
+    @Override
+    public String getJoinPage(HttpServletRequest request, HttpServletResponse response, Model model) {
+        model.addAttribute(JoinRequest.builder().build());
+        return "join";
+    }
+
+    @Override
+    public String join(@ModelAttribute JoinRequest joinRequest, HttpServletRequest request, HttpServletResponse response) {
+        JoinParam param = JoinParam.builder()
+                .email(joinRequest.getEmail())
+                .password(joinRequest.getPassword())
+                .build();
+
+        memberService.join(param);
+        return "index";
+    }
 }
