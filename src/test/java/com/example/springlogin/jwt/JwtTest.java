e319ed1fabd39e8cee6246191484277328af4b89
bluepeng9 <bluepeng9@gmail.com>
2023-10-10 22:42:33 +0900

    chore: jwt 라이브러리 추가

---
+++ src/test/java/com/example/springlogin/jwt/JwtTest.java
@@ -0,0 +1,14 @@
+package com.example.springlogin.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JwtTest {
+
+    @Test
+    void jwt_토큰_생성() {
+
+    }
+
+}
