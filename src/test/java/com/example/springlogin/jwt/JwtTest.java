3e0a1d7330b4c606136c6d2b8574bfd75d679b08
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-10-11 22:59:01 +0900

    test: claim 만드는 함수 구현

--- src/test/java/com/example/springlogin/jwt/JwtTest.java
+++ src/test/java/com/example/springlogin/jwt/JwtTest.java
@@ -19,22 +19,21 @@ public class JwtTest {
         Member member = new Member("test@naver.com", "password");
         ReflectionTestUtils.setField(member, "id", 1L);
 
-        Claims claim = Jwts.claims();
-        claim.put("userId", member.getId());
-
         String secret = Base64.getEncoder().encodeToString("asdfasdfdasf".getBytes());
         String token = Jwts.builder()
-                .setClaims(claim)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 60 * 1000))
+                .setClaims(createClaims(member.getId()))
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
 
         System.out.println("token = " + token);
     }
 
-    Claims createClaims() {
-        return Jwts.claims();
+    Claims createClaims(Long id) {
+        Claims claims = Jwts.claims()
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 60 * 1000));
+        claims.put("userID", id);
+        return claims;
     }
 
 }
