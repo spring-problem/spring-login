c9dd72e8895cee72234d99a86b2a18dfcb2967b6
junyoung-choe <wjdghkswnsdu@naver.com>
2023-10-12 00:00:24 +0900

    test: createHeader 메소드 구현

--- src/test/java/com/example/springlogin/jwt/JwtTest.java
+++ src/test/java/com/example/springlogin/jwt/JwtTest.java
@@ -2,6 +2,7 @@ package com.example.springlogin.jwt;
 
 import com.example.springlogin.member.domain.Member;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 public class JwtTest {
@@ -21,10 +24,11 @@ public class JwtTest {
 
         String secret = Base64.getEncoder().encodeToString("asdfasdfdasf".getBytes());
         String token = Jwts.builder()
+                .setHeader(createHeader())
                 .setClaims(createClaims(member.getId()))
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
-
+        System.out.println(secret);
         System.out.println("token = " + token);
     }
 
@@ -34,6 +38,17 @@ public class JwtTest {
                 .setExpiration(new Date(System.currentTimeMillis() + 60 * 1000));
         claims.put("userID", id);
         return claims;
+    }
+
+    Map<String, Object> createHeader() {
+        String alg = "HS256";
+        String typ = "JWT";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("alg" , alg);
+        map.put("typ" , typ);
+
+        return map;
     }
 
 }
