a564429af9e06b45bb8fea183dd6672ec03f5c08
bluepeng9 <bluepeng9@gmail.com>
2023-10-12 22:29:42 +0900

    feat: TokenProvider 추가

---
+++ src/main/java/com/example/springlogin/global/util/TokenProvider.java
@@ -0,0 +1,16 @@
+package com.example.springlogin.global.util;
+
+import com.example.springlogin.member.domain.Member;
+
+public class TokenProvider {
+
+
+    public void generateToken(Member member) {
+    }
+
+    void createClaims(Long id) {
+    }
+
+    void createHeader() {
+    }
+}
