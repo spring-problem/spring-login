5088cab9f935dfdfa84fe4e75407994b0c284a97
juhyun-99 <juhyeon042@naver.com>
2023-10-15 16:02:59 +0900

    refactor: 피드백 내용 기반으로 수정

--- src/main/java/com/example/springlogin/global/util/TokenProvider.java
+++ src/main/java/com/example/springlogin/global/util/TokenProvider.java
@@ -4,10 +4,12 @@ import com.example.springlogin.member.domain.Member;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Base64;
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,30 +18,29 @@ import java.util.Map;
 public class TokenProvider {
     private final SignatureAlgorithm alg = SignatureAlgorithm.valueOf("HS256");
     private final String typ = "JWT";
-    private final String secret;
-    private final long expireTime;
+    private final SecretKey secret;
+    private final long expireTimeMilliSecond;
 
     public TokenProvider(@Value("${jwt.secret}") String secret,
                          @Value("${jwt.token-validity-in-seconds}") String expireTime){
-        this.secret = Base64.getEncoder().encodeToString(secret.getBytes());
-        this.expireTime = Long.parseLong(expireTime);
+        this.secret = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+        this.expireTimeMilliSecond = Long.parseLong(expireTime) * 1000;
     }
 
     public String generateToken(Member member) {
         String token = Jwts.builder()
                 .setHeader(createHeader())
-                .setSubject(String.valueOf(member.getId()))
-                .signWith(SignatureAlgorithm.HS256, secret)
+                .setClaims(createClaims(member.getId()))
+                .signWith(secret, SignatureAlgorithm.HS256)
                 .compact();
-
         return token;
     }
 
     Claims createClaims(Long id) {
         Claims claims = Jwts.claims()
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + expireTime));
-        claims.put("userID", id);
+                .setExpiration(new Date(System.currentTimeMillis() + expireTimeMilliSecond))
+                .setSubject(String.valueOf(id));
         return claims;
     }
 
