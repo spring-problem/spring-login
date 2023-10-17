9efaadb46ea759009ddf0fcda83016c6429ad301
junyoung-choe <wjdghkswnsdu@naver.com>
2023-10-18 00:11:50 +0900

    test: jwt 토큰 decode와 검증 테스트

--- src/test/java/com/example/springlogin/jwt/JwtTest.java
+++ src/test/java/com/example/springlogin/jwt/JwtTest.java
@@ -3,13 +3,17 @@ package com.example.springlogin.jwt;
 import com.example.springlogin.global.util.TokenProvider;
 import com.example.springlogin.member.domain.Member;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import javax.crypto.SecretKey;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
@@ -64,6 +68,33 @@ public class JwtTest {
         map.put("typ" , typ);
 
         return map;
+    }
+
+    @Test
+    public void 토큰_payload값_가져오기(){
+        Member member = new Member("test@naver.com", "password");
+        ReflectionTestUtils.setField(member, "id", 1L);
+        String token = provider.generateToken(member);
+
+        String secret =  "dGhpc0lzTXlTZWNyZXRLZXlNYWRlQnlKdWh5dW5Tb24=";
+
+        String result = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+
+        // 설정한 PK 값 가져온다 !
+        System.out.println(result);
+    }
+
+    @Test
+    public void 토큰_검증() {
+        // 클라이언트가 보내온 토큰값 !
+        Member member = new Member("test@naver.com", "password");
+        ReflectionTestUtils.setField(member, "id", 1L);
+        String token = provider.generateToken(member);
+
+        String secret =  "dGhpc0lzTXlTZWNyZXRLZXlNYWRlQnlKdWh5dW5Tb24=";
+
+        Jws<Claims> claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+        System.out.println(!claims.getBody().getExpiration().before(new Date()));
     }
 
 }
