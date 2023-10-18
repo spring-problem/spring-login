5b8e7f2560b02d23e8add2eaf901b78f301d5595
bluepeng9 <bluepeng9@gmail.com>
2023-10-18 10:55:22 +0900

    feat: 인증 에러 예외 클래스 추가

---
+++ src/main/java/com/example/springlogin/global/exception/AuthException.java
@@ -0,0 +1,7 @@
+package com.example.springlogin.global.exception;
+
+public class AuthException extends RuntimeException{
+    public AuthException(Throwable cause) {
+        super(cause);
+    }
+}
