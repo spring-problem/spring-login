170b10f5e43d495a11547442428c59254be83f4f
junyoung-choe <wjdghkswnsdu@naver.com>
2023-12-05 19:32:03 +0900

    feat: member List page 추가

---
+++ src/main/java/com/example/springlogin/global/exception/NotImplementedException.java
@@ -0,0 +1,7 @@
+package com.example.springlogin.global.exception;
+
+public class NotImplementedException extends RuntimeException {
+
+    public NotImplementedException(String message) {super(message);}
+
+}
