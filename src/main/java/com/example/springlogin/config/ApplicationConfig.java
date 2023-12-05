325252997cba7977f83d31e3ad14662f49b0faeb
bluepeng9 <bluepeng9@gmail.com>
2023-12-05 16:09:20 +0900

    feat: admin 더미 데이터 추가

--- src/main/java/com/example/springlogin/config/ApplicationConfig.java
+++ src/main/java/com/example/springlogin/config/ApplicationConfig.java
@@ -1,10 +1,26 @@
 package com.example.springlogin.config;
 
 import com.example.springlogin.config.auth.JwtAuthConfig;
+import com.example.springlogin.member.domain.Member;
+import com.example.springlogin.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.event.EventListener;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @Import(JwtAuthConfig.class)
+@RequiredArgsConstructor
 public class ApplicationConfig {
+
+    final MemberRepository memberRepository;
+
+    @EventListener(ApplicationReadyEvent.class)
+    @Transactional
+    public void init() {
+        Member admin = new Member("admin@a.com", "admin");
+        memberRepository.save(admin);
+    }
 }
