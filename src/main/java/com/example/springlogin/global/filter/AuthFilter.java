6381730a74e99e897e6db082629d790b377bc452
bluepeng9 <bluepeng9@gmail.com>
2023-12-06 18:21:50 +0900

    feat: AuthFilter 추가

---
+++ src/main/java/com/example/springlogin/global/filter/AuthFilter.java
@@ -0,0 +1,39 @@
+package com.example.springlogin.global.filter;
+
+import com.example.springlogin.global.util.auth.AuthUtil;
+import com.example.springlogin.member.domain.Member;
+import com.example.springlogin.member.domain.Role;
+import com.example.springlogin.member.service.MemberService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class AuthFilter extends OncePerRequestFilter {
+
+    final AuthUtil authUtil;
+    final MemberService memberService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        Long memberId = authUtil.getMemberId(request);
+        Optional<Member> loginUserById = memberService.getLoginUserById(memberId);
+        if (loginUserById.isEmpty()) {
+            throw new RuntimeException("유저가 존재하지 않습니다.");
+        }
+        Member findMember = loginUserById.get();
+
+        if (!Role.ADMIN.equals(findMember.getRole())) {
+            throw new RuntimeException("접근 권한이 없습니다.");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
