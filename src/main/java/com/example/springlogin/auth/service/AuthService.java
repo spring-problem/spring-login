40c16e33ba5d32149eb2f7ff87fff573823e9899
junyoung-choe <wjdghkswnsdu@naver.com>
2023-12-11 17:18:40 +0900

    feat: refreshToken delete in service 구현

--- src/main/java/com/example/springlogin/auth/service/AuthService.java
+++ src/main/java/com/example/springlogin/auth/service/AuthService.java
@@ -5,6 +5,7 @@ import com.example.springlogin.auth.repository.AuthRepository;
 import com.example.springlogin.auth.service.param.GenerateRefreshTokenParam;
 import com.example.springlogin.member.domain.Member;
 import com.example.springlogin.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,10 +15,12 @@ import java.util.Optional;
 @Service
 @Transactional(readOnly = true)
 @Slf4j
+@RequiredArgsConstructor
 public class AuthService {
 
-    AuthRepository authRepository;
-    MemberRepository memberRepository;
+    final AuthRepository authRepository;
+    final MemberRepository memberRepository;
+
 
     @Transactional(readOnly = false)
     public void generateRefreshToken(GenerateRefreshTokenParam param) {
@@ -25,5 +28,12 @@ public class AuthService {
         RefreshToken refreshToken = RefreshToken.createRefreshToken(member.get(), param.getToken());
         authRepository.save(refreshToken);
         log.info("token 등록 완료");
+    }
+
+    @Transactional(readOnly = false)
+    public void deleteRefreshToken(GenerateRefreshTokenParam param) {
+        Long memberId = param.getMemberId();
+        authRepository.아무거나(memberId);
+
     }
 }
