bc7d9cf89534825097d4ff143f877d36902e4f78
junyoung-choe <wjdghkswnsdu@naver.com>
2023-12-11 17:18:32 +0900

    feat: refreshToken delete in repository 구현

--- src/main/java/com/example/springlogin/auth/repository/AuthRepository.java
+++ src/main/java/com/example/springlogin/auth/repository/AuthRepository.java
@@ -2,8 +2,13 @@ package com.example.springlogin.auth.repository;
 
 import com.example.springlogin.auth.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthRepository extends JpaRepository<RefreshToken, Long> {
+    @Modifying
+    @Query(value = "delete from RefreshToken where member.id =:member")
+    public void 아무거나(Long member);
 }
