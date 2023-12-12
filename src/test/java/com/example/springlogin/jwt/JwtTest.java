8f067bc7ea1f479ca4ca3b86f2cba6885ed795e3
bluepeng9 <bluepeng9@gmail.com>
2023-12-12 18:26:20 +0900

    test: 로그인 시 token 발급 확인

--- src/test/java/com/example/springlogin/jwt/JwtTest.java
+++ src/test/java/com/example/springlogin/jwt/JwtTest.java
@@ -3,26 +3,28 @@ package com.example.springlogin.jwt;
 import com.example.springlogin.domain.member.domain.Member;
 import com.example.springlogin.global.exception.JwtParsingFailException;
 import com.example.springlogin.global.util.TokenProvider;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.util.ReflectionTestUtils;
-
-import java.util.Base64;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @ActiveProfiles("token-provider-test")
+@AutoConfigureMockMvc
 public class JwtTest {
 
     private final TokenProvider provider;
+
+    @Autowired
+    MockMvc mvc;
 
     @Autowired
     public JwtTest(TokenProvider provider) {
@@ -35,6 +37,22 @@ public class JwtTest {
         ReflectionTestUtils.setField(member, "id", 1L);
         System.out.println(provider.generateToken(member.getId()));
     }
+
+    @Test
+    void 로그인_시_refreshtoken_발급_확인(
+            @Value("${auth.jwt.access-cookie-key}") String accessCookieName,
+            @Value("${auth.jwt.refresh-cookie-key}") String refreshCookieName
+    ) throws Exception {
+        MvcResult mvcResult = mvc.perform(
+                MockMvcRequestBuilders.post("/login")
+                        .contentType("application/x-www-form-urlencoded")
+                        .content("email=admin%40a.com&password=admin")
+        ).andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String accessToken = response.getCookie(accessCookieName).getValue();
+        String refreshToken = response.getCookie(refreshCookieName).getValue();
+    }
+
 
     @Test
     void TokenProvider_validate_메서드_테스트() throws InterruptedException {
