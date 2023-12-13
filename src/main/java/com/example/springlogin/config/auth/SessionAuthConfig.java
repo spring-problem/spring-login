d25e5bea2974019721ed8e53b32bf09a7b43a245
junyoung-choe <wjdghkswnsdu@naver.com>
2023-12-13 23:11:44 +0900

    feat: AuthExceptionHandler bean 등록

--- src/main/java/com/example/springlogin/config/auth/SessionAuthConfig.java
+++ src/main/java/com/example/springlogin/config/auth/SessionAuthConfig.java
@@ -3,10 +3,14 @@ package com.example.springlogin.config.auth;
 import com.example.springlogin.domain.member.controller.MemberController;
 import com.example.springlogin.domain.member.controller.MemberSessionController;
 import com.example.springlogin.domain.member.service.MemberService;
+import com.example.springlogin.global.exception.handler.AuthExceptionHandler;
+import com.example.springlogin.global.exception.handler.CookieAuthExceptionHandler;
+import com.example.springlogin.global.exception.handler.SessionAuthExceptionHandler;
 import com.example.springlogin.global.filter.AuthFilter;
 import com.example.springlogin.global.util.auth.AuthUtil;
 import com.example.springlogin.global.util.auth.SessionAuthUtil;
 import jakarta.servlet.Filter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +20,8 @@ import java.util.List;
 @Configuration
 public class SessionAuthConfig {
     @Bean
-    MemberController memberController(MemberService memberService) {
-        return new MemberSessionController(memberService);
+    MemberController memberController(MemberService memberService, @Value("${session.loginBySession}")String loginBySession) {
+        return new MemberSessionController(memberService, loginBySession);
     }
 
     @Bean
@@ -42,4 +46,8 @@ public class SessionAuthConfig {
         return bean;
     }
 
+    @Bean
+    AuthExceptionHandler authExceptionHandler( @Value("${session.loginBySession}")String loginBySession) {
+        return new SessionAuthExceptionHandler(loginBySession);
+    }
 }
