6309a9c37ad4600ffc7603796bc118a3176a604f
bluepeng9 <bluepeng9@gmail.com>
2023-12-13 18:30:06 +0900

    feat: ExceptionControllerAdvice 추가

--- src/main/java/com/example/springlogin/global/advice/ExceptionControllerAdvice.java
+++ src/main/java/com/example/springlogin/global/advice/ExceptionControllerAdvice.java
@@ -1,14 +1,22 @@
 package com.example.springlogin.global.advice;
 
-import com.example.springlogin.global.exception.JwtParsingFailException;
+import com.example.springlogin.global.exception.AuthException;
+import com.example.springlogin.global.exception.handler.AuthExceptionHandler;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@RequiredArgsConstructor
 public class ExceptionControllerAdvice {
 
-    @ExceptionHandler(JwtParsingFailException.class)
-    public String handleJwtParsingFailException() {
+    final AuthExceptionHandler authExceptionHandler;
+
+    @ExceptionHandler(AuthException.class)
+    public String handleAuthException(HttpServletRequest request, HttpServletResponse response) {
+        authExceptionHandler.handleAuthException(request, response);
         return "index";
     }
 }
