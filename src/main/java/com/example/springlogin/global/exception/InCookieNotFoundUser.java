e091f039f967601f31fb57f32720df7acc249db1
junyoung-choe <wjdghkswnsdu@naver.com>
2023-12-13 22:40:35 +0900

    refactor: 호출 하는 부분 exception 이름 변경

--- src/main/java/com/example/springlogin/global/exception/InCookieNotFoundUser.java
+++
@@ -1,9 +0,0 @@
-package com.example.springlogin.global.exception;
-
-import com.example.springlogin.global.exception.handler.AuthExceptionHandler;
-
-public class InCookieNotFoundUser extends AuthException {
-    public InCookieNotFoundUser(Throwable cause) {
-        super(cause);
-    }
-}
--- src/main/java/com/example/springlogin/global/exception/handler/CookieAuthExceptionHandler.java
+++ src/main/java/com/example/springlogin/global/exception/handler/CookieAuthExceptionHandler.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class CookieAuthExceptionHandler implements  AuthExceptionHandler{
-    // advide 는 그냥 에러만 잡아 온다 ! (AuthException 의 오류를 잡아오고 InCookieNotFoundUser(Cookie controller 에 있는) 는 상속 받았으니 에러가 advice 에서 잡힌다)
+    // advide 는 그냥 에러만 잡아 온다 ! (AuthException 의 오류를 잡아오고 CookieAuthException(Cookie controller 에 있는) 는 상속 받았으니 에러가 advice 에서 잡힌다)
     // advice는 AuthExceptionHandler 인터페이스를 호출하고 Configuration 에서 등록된 구현체인 CookieAuthExceptionHandler 로 오버라이드 되서 이 함수가 호출된다 여기서 로직을 구현
     private final String loginCookieName;
 
